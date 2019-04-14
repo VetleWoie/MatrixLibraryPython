ffc38db255f252e30b46aa99aefe6f808e9ec263
vetlewoie <vet.h.woie@hotmail.com>
2019-04-14 12:48:02 +0200

    Fixed scalar multiplication

--- Matrix.py
+++ Matrix.py
@@ -14,23 +14,29 @@ class Matrix():
             self.rows = len(matrix[0])
             self.cols = len(matrix)
 
-    def matmul(A, B):
+    def matmul(self, other):
         if(self.cols != self.rows):
             raise ValueError("Matrix multiplication: Columns != Rows")
-        new = Matrix(self.rows, other.cols)
+        new = Matrix(rows=self.rows, columns = other.cols)
         for i in range(0,self.rows-1):
             for j in range(0,other.cols-1):
                 for k in range(0,self.cols-1):
                     new.mat[i][j] += self.mat[k][i]*other.mat[j][k]
         return new
     
-    def scalmul(A,B):
-        
+    def scalmul(self,other):
+        new = Matrix(rows=self.rows,columns=self.cols)
+        for i in range(0,self.rows):
+            for j in range(0,self.cols):
+                new.mat[i][j] = self.mat[i][j] * other
+        return new
+
 
     def __mul__(self,other):
-        if 
+        if type(other) == type(1):
+            return self.scalmul(other)
+        else:
+            return self.matmul(other)
 
     def __add__(self,other):
-
-        
-        
+        pass
