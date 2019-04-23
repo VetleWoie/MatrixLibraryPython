4b7c8a150ce26691fcad73af70c0831706a4514e
vet.h.woie@hotmail.com <vet.h.woie@hotmail.com>
2019-04-23 16:34:56 +0200

    minor syntax errors fixed

--- Tesseract.py
+++ Tesseract.py
@@ -12,17 +12,17 @@ class Simulation:
         self.screen = pygame.display.set_mode(self.size)
 
         self.project = Matrix(matrix=[  [1,0,0],
-                                        [0,1,0] )
+                                        [0,1,0] ])
 
         self.cube = [
-                        Matrix(matrix=[[0],[0],[0]]) #1
-                        Matrix(matrix=[[1],[0],[0]]) #2
-                        Matrix(matrix=[[1],[1],[0]]) #3
-                        Matrix(matrix=[[0],[1],[0]]) #4
-                        Matrix(matrix=[[0],[0],[1]]) #5
-                        Matrix(matrix=[[1],[0],[1]]) #6
-                        Matrix(matrix=[[1],[1],[1]]) #7
-                        Matrix(matrix=[[0],[1],[1]]) #8
+                        Matrix(matrix=[[0],[0],[0]]), #1
+                        Matrix(matrix=[[1],[0],[0]]), #2
+                        Matrix(matrix=[[1],[1],[0]]), #3
+                        Matrix(matrix=[[0],[1],[0]]), #4
+                        Matrix(matrix=[[0],[0],[1]]), #5
+                        Matrix(matrix=[[1],[0],[1]]), #6
+                        Matrix(matrix=[[1],[1],[1]]), #7
+                        Matrix(matrix=[[0],[1],[1]])  #8
                     ]
 
         self.simulate()
