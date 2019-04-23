3367e34812b80fa56475b294584420ed6dbdfa84
vetlewoie <vet.h.woie@hotmail.com>
2019-04-23 13:57:10 +0200

    Fixed various minor problems

--- Matrix.py
+++ Matrix.py
@@ -1,3 +1,5 @@
+import code
+
 class Matrix():
 
     def __init__(self, rows = False, columns = False, matrix = False):
@@ -5,9 +7,9 @@ class Matrix():
             self.rows = rows
             self.cols = columns
             self.mat = []
-            for i in range(0,self.cols-1):
+            for i in range(0,self.cols):
                 self.mat.append([])
-                for  j in range(0,self.rows-1):
+                for  j in range(0,self.rows):
                     self.mat[i].append(0)
         elif(matrix):
             self.mat = matrix
@@ -18,18 +20,27 @@ class Matrix():
         if(self.cols != self.rows):
             raise ValueError("Matrix multiplication: Columns != Rows")
         new = Matrix(rows=self.rows, columns = other.cols)
-        for i in range(0,self.cols-1):
-            for j in range(0,other.rows-1):
-                for k in range(0,self.cols-1):
+        for i in range(0,self.cols):
+            for j in range(0,other.rows):
+                for k in range(0,self.cols):
                     new.mat[i][j] += self.mat[i][k]*other.mat[k][j]
         return new
     
     def scalmul(self,other):
         new = Matrix(rows=self.rows,columns=self.cols)
-        for i in range(0,self.cols-1):
-            for j in range(0,self.rows-1):
+        for i in range(0,self.cols):
+            for j in range(0,self.rows):
                 new.mat[i][j] = self.mat[i][j] * other
         return new
+
+    def Trace(self):
+        if(self.rows != self.cols):
+            raise ValueError("Trace can only be done on square matrix")
+
+        sum = 0
+        for i in range(0,self.rows-1):
+            sum += self.mat[i][i]
+        return sum
 
 
     def __mul__(self,other):
@@ -51,15 +62,15 @@ class Matrix():
         if self.rows != other.rows and self.cols != other.cols:
             raise ValueError("Matrixes has to be of same size for addition")
         new = Matrix(rows = self.rows, columns = self.cols)
-        for i in range(0,self.cols-1):
-            for j in range(0,self.rows-1):
+        for i in range(0,self.cols):
+            for j in range(0,self.rows):
                 new.mat[i][j] = self.mat[i][j] + other.mat[i][j]
     
     def __iadd__(self, other):
         if self.rows != other.rows and self.cols != other.cols:
             raise ValueError("Matrixes has to be of same size for addition")
-        for i in range(0,self.cols-1):
-            for j in range(0,self.rows-1):
+        for i in range(0,self.cols):
+            for j in range(0,self.rows):
                 self.mat[i][j] += other.mat[i][j]
     
     def __sub__(self, other):
@@ -67,7 +78,24 @@ class Matrix():
     
     def __isub__(self, other):
         self.__iadd__(other * -1)
+    
+    def __repr__(self):
+        return f'Matrix {self.rows} x {self.cols}, with {repr(self.mat[0][0])} Elements'
+    
+    def __str__(self):
+        str = ''
+        for  i in range(0,self.cols):
+            str = str + '[\t'
+            for j in range(0, self.rows):
+                str = str + f'{self.mat[i][j]}' + '\t'
+            str = str + ']\n'
+        return str 
 
 class Vector(Matrix):
     def __init__(self,dim):
-        Super.__init__(rows = dim, columns = 1)
+        super.__init__(rows = dim, columns = 1)
+
+
+x = Matrix(matrix=[[3,4,5],[4,5,6],[5,6,7]])
+y = Matrix(matrix=[[1,2,3],[2,3,4,],[4,5,6]])
+code.interact(local=locals())
