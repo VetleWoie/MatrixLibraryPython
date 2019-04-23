9f263d496e455193f608cef2e3e94c67de990574
vet.h.woie@hotmail.com <vet.h.woie@hotmail.com>
2019-04-23 16:33:53 +0200

    Started working on visualizing more dimensions

---
+++ Tesseract.py
@@ -0,0 +1,43 @@
+import pygame
+from Matrix import Matrix
+from Matrix import Vector
+import math
+
+
+
+class Simulation:
+    def __init__(self,width, height):
+        pygame.init()
+        self.size = (width, height)
+        self.screen = pygame.display.set_mode(self.size)
+
+        self.project = Matrix(matrix=[  [1,0,0],
+                                        [0,1,0] )
+
+        self.cube = [
+                        Matrix(matrix=[[0],[0],[0]]) #1
+                        Matrix(matrix=[[1],[0],[0]]) #2
+                        Matrix(matrix=[[1],[1],[0]]) #3
+                        Matrix(matrix=[[0],[1],[0]]) #4
+                        Matrix(matrix=[[0],[0],[1]]) #5
+                        Matrix(matrix=[[1],[0],[1]]) #6
+                        Matrix(matrix=[[1],[1],[1]]) #7
+                        Matrix(matrix=[[0],[1],[1]]) #8
+                    ]
+
+        self.simulate()
+    
+    def simulate(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+        
+        
+            
+Simulation(320,240)
+
+
+
+
+
