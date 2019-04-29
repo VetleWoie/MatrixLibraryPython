f393e65942485bdbe69966999ebef70b940e359f
vetlewoie <vet.h.woie@hotmail.com>
2019-04-29 15:48:52 +0200

    Made lines work in 3 dimensions

--- Tesseract.py
+++ Tesseract.py
@@ -12,6 +12,7 @@ BLACK = (0,0,0)
 
 SCALE = 200
 DOTSIZE = 2
+WIDTH = 2
 
 
 
@@ -24,9 +25,7 @@ class Simulation:
         self.dim = dim
         self.clock = pygame.time.Clock()
 
-        self.projections = []
-        for i in reversed(range(3,dim+1)):
-            self.projections.append(self.project(i,i-1))
+        self.projection = self.project(dim,2)
         self.project(dim,dim-1)
         self.cube = self.box(dim)
 
@@ -62,7 +61,9 @@ class Simulation:
     def simulate(self):
         draw = False
         angle = 0
+        lines = False
         while True:
+            points = []
             if not draw:
                 self.screen.fill(BLACK)
             for event in pygame.event.get():
@@ -71,13 +72,21 @@ class Simulation:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         draw = not draw
+                    elif event.key == pygame.K_l:
+                        lines = not lines
             for point in self.cube:
                 for i in range(self.dim-1):
                     point = self.rotate(self.dim,angle,i,i+1) * point
                 point = point * self.scale
-                for i in self.projections:
-                    point = i * point  
-                pygame.draw.circle(self.screen,RED,(int(point.mat[0][0])+int(self.size[0]/2),int(point.mat[1][0])+int(self.size[1]/2)), DOTSIZE)
+                point = self.projection * point 
+                point.mat[0][0] = int(point.mat[0][0] + self.size[0]/2)
+                point.mat[1][0] = int(point.mat[1][0] + self.size[1]/2)
+                points.append(point)
+
+            if lines:
+                self.lines(points,RED)
+            for i in points:
+                pygame.draw.circle(self.screen,RED,(i.mat[0][0],i.mat[1][0]),DOTSIZE)
             
             pygame.display.update()
 
@@ -103,12 +112,28 @@ class Simulation:
                 rotation.mat[i][i] = 1 
         return rotation
 
-    def lines(self,cube):
+    def lines(self,points,color):
+        for i in range(0,len(points),4):
+            p1 = (points[i].mat[0][0] ,  points[i].mat[1][0])
+            p2 = (points[i+1].mat[0][0] ,  points[i+1].mat[1][0])
+            p3 = (points[i+2].mat[0][0] , points[i+2].mat[1][0])
+            p4 = (points[i+3].mat[0][0] , points[i+3].mat[1][0])
+            pygame.draw.line(self.screen,color,p1,p2,WIDTH)
+            pygame.draw.line(self.screen,color,p1,p3,WIDTH)
+            pygame.draw.line(self.screen,color,p2,p4,WIDTH)
+            pygame.draw.line(self.screen,color,p3,p4,WIDTH)
 
-        pass
+        for j in range(3,self.dim+1):
+            for i in range(int(len(points)/2)):
+                p1 = (points[i].mat[0][0] ,  points[i].mat[1][0])
+                p2 = (points[i+(2**(j-1))].mat[0][0] ,  points[i+(2**(j-1))].mat[1][0])
+                pygame.draw.line(self.screen,color,p1,p2,WIDTH)
+            
+
+            
         
             
-Simulation(500,500,4)
+Simulation(500,500,2)
 
 
 
